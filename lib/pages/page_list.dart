7ef2ad499dfc65c98f20f7617b6107be5b3db50a
asbp <guungofficial@gmail.com>
2021-01-31 15:34:36 +0700

    fix error

--- lib/pages/page_list.dart
+++ lib/pages/page_list.dart
@@ -25,7 +25,8 @@ class _PageListState extends State<PageList> {
 
   Future<List<Pasien>> loadPasien() async {
     return await nutil
-        .get("${REST_URL}/pasien")
+        .get("$REST_URL/pasien")
+        .catchError((onError) => print("Error lagi"))
         .then((value) => json2List(value));
   }
 
